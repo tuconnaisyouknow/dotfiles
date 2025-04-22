8db675bcc206a3933c4109af1689f0a5f6a23f5d
tuconnaisyouknow <yenghele@yahoo.com>
2025-04-22 10:25:41 +0200

    fixing script to not break gnu stow symlink

--- scripts/Scripts/monitor-switch.sh
+++ scripts/Scripts/monitor-switch.sh
@@ -1,34 +1,29 @@
 #!/bin/bash
 
-# Fichier à modifier (remplace ce chemin si besoin)
-CONFIG_FILE="/home/tuconnais/.config/hypr/monitors.conf"
+CONFIG_FILE="$HOME/.config/hypr/monitors.conf"
 
-if [[ "$1" != "home" && "$1" != "work" ]]; then
+[[ "$1" != "home" && "$1" != "work" ]] && {
   echo "Usage: $0 [home|work]"
   exit 1
-fi
-
-# On lit le fichier et modifie les lignes
-awk -v mode="$1" '
-/^# Home/ { home=1; work=0; print; next }
-/^# Work/ { home=0; work=1; print; next }
-
-home {
-  if (mode == "home") {
-    sub(/^#/, ""); print
-  } else {
-    if ($0 !~ /^#/) $0="#" $0; print
-  }
-  next
 }
 
-work {
-  if (mode == "work") {
-    sub(/^#/, ""); print
-  } else {
-    if ($0 !~ /^#/) $0="#" $0; print
-  }
-  next
-}
+mode="$1"
+new_content=$(awk -v mode="$mode" '
+  /^# Home/ { in_home=1; in_work=0; print; next }
+  /^# Work/ { in_home=0; in_work=1; print; next }
 
-{ print }' "$CONFIG_FILE" > "${CONFIG_FILE}.tmp" && mv "${CONFIG_FILE}.tmp" "$CONFIG_FILE"
+  in_home {
+    if (mode == "home") sub(/^#/, ""); else if ($0 !~ /^#/) $0="#" $0
+    print; next
+  }
+
+  in_work {
+    if (mode == "work") sub(/^#/, ""); else if ($0 !~ /^#/) $0="#" $0
+    print; next
+  }
+
+  { print }
+' "$CONFIG_FILE")
+
+# ✍️ Écriture in-place (le fichier reste intact, le symlink aussi)
+printf "%s\n" "$new_content" >"$CONFIG_FILE"
