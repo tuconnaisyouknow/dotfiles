6cddd7f9e60224afc439b517b57d9c96d4d05e85
tuconnaisyouknow <yenghele@yahoo.com>
2025-08-12 12:17:14 +0200

    fixing some missing icons

--- scripts/Scripts/volume.sh
+++ scripts/Scripts/volume.sh
@@ -5,8 +5,8 @@ volume_raw=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ 2>/dev/null | grep -i "Volume
 
 # Si le son est muet
 if echo "$volume_raw" | grep -q '\[MUTED\]'; then
-    echo -e " Mute"
-    exit 0
+  echo -e "  Mute"
+  exit 0
 fi
 
 # Extrait le volume float (ex: 0.83)
@@ -16,15 +16,15 @@ volume_float=$(echo "$volume_raw" | awk '{ print $2 }')
 volume_int=$(echo "$volume_float * 100" | bc -l | awk '{ printf "%d", $1 }')
 
 # Convertit point en virgule (même si inutile ici car entier, mais pour cohérence FR)
-volume_fr=$(echo "$volume_int" | sed 's/\./,/' )
+volume_fr=$(echo "$volume_int" | sed 's/\./,/')
 
 # Choix de l'icône selon le volume
 if [ "$volume_int" -ge 66 ]; then
-    icon=" "
+  icon=" "
 elif [ "$volume_int" -ge 33 ]; then
-    icon=" "
+  icon=" "
 else
-    icon=" "
+  icon=" "
 fi
 
 # Affichage final 🇫🇷
