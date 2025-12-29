2786db3011b9751b4d826a78e376824d98e2f872
tuconnaisyouknow <yenghele@yahoo.com>
2025-12-29 11:10:20 +0100

    resolving some colorscheme problem in neovim

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,23 @@
 -- lua/plugins/rose-pine.lua
 return {
-  "rose-pine/neovim",
-  name = "rose-pine",
-  config = function()
-    require("rose-pine").setup({
-      variant = "moon",
-      dark_variant = "moon",
-    })
-    vim.cmd("colorscheme rose-pine")
-  end
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    priority = 1000,
+    config = function()
+      require("rose-pine").setup({
+        variant = "moon",
+        dark_variant = "moon",
+      })
+      vim.cmd("colorscheme rose-pine")
+    end,
+  },
+  {
+    "folke/tokyonight.nvim",
+    enabled = false,
+  },
+  {
+    "catppuccin/nvim",
+    enabled = false,
+  },
 }
