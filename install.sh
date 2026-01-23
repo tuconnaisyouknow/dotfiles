4c1a4902ce1c12642bbca9f45fcb8d5ce1f48e65
tuconnaisyouknow <yenghele@yahoo.com>
2026-01-23 22:24:31 +0100

    fixing oh my zsh plugin installation in install.sh script

--- install.sh
+++ install.sh
@@ -50,10 +50,10 @@ papirus-folders -C cat-mocha-mauve --theme Papirus-Dark
 # Installing oh my zsh and its plugins
 [[ -d ~/.oh-my-zsh ]] && mv ~/.oh-my-zsh ~/.backup
 RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
-git clone https://github.com/MichaelAquilina/zsh-you-should-use.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/you-should-use"
-git clone https://github.com/fdellwing/zsh-bat.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-bat"
+git clone https://github.com/zsh-users/zsh-autosuggestions "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
+git clone https://github.com/MichaelAquilina/zsh-you-should-use.git "$HOME/.oh-my-zsh/custom/plugins/you-should-use"
+git clone https://github.com/fdellwing/zsh-bat.git "$HOME/.oh-my-zsh/custom/plugins/zsh-bat"
 
 # Creating required folders
 mkdir -p ~/.backup ~/.dotfiles
